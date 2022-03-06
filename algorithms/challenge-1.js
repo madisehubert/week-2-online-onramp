e8b71aa953d860e5eb4eb55f3000b5d87c21b345
Hubert Ovie Madise <54346410+madisehubert@users.noreply.github.com>
2022-03-06 17:13:00 +0100

    Update challenge-1.js

--- algorithms/challenge-1.js
+++ algorithms/challenge-1.js
@@ -1,3 +1,36 @@
-function missingNumber(numArr) {}
+// # Challenge 1
 
-module.exports = missingNumber
+// Implement the function missingNumber(numArr)
+// which returns the missing number in a given
+// integer array between 1 and 9. If there is no
+// missing number, the function should return
+// false. If there is more than one missing number,
+//  the function should return the result in an
+//  array sorted in ascending order.
+
+function missingNumber(numArr) {
+  // Array of integer between 1 and 9
+  const integerArr = [1, 2, 3, 4, 5, 6, 7, 8, 9];
+  //Array containing missing numbers
+  let missingNumArr = [];
+  //iterate through element s
+  for (let k of integerArr) {
+    //check if number in numArr is also in Integere arr
+    if (numArr.includes(k) === false) {
+      missingNumArr.push(k);
+    }
+  }
+  //conditional statements and results
+
+  if (missingNumArr.length === 0) {
+    return false;
+  } else if (missingNumArr.length === 1) {
+    let missingNum = missingNumArr[0];
+    return missingNum;
+  } else {
+    return missingNumArr.sort();
+  }
+}
+
+module.exports = missingNumber;
+
